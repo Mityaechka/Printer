e6787aacda3c38613ed50b76f6f278a8614299bd
DmitriyPavlov <pavlov32sh1@gmail.com>
2020-10-13 14:45:28 +0600

    fix

--- PrinterExtension/content.js
+++ PrinterExtension/content.js
@@ -10,13 +10,7 @@ function SendMessage(message) {
 }
 
 document.addEventListener("printCanvas", function (data) {
-    
-    //var canvas = document.getElementById("canvas");
-    //var dataURL = canvas.toDataURL("image/png");
-   // dataURL = dataURL.replace('data:image/png;base64,', '');
-
     message = data.detail;
     console.log(message);
     chrome.extension.sendMessage(message);
-
 });
